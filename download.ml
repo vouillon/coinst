aaa9effbd9ce691d7e9f345a42c0c2e5c0f87db6
jerome.vouillon <jerome.vouillon@pps.jussieu.fr>
2011-11-15 23:18:12 +0000

    Improved Debian control file downloading script

--- download.ml
+++ download.ml
@@ -10,20 +10,25 @@ let britney_dir = Filename.concat dest "britney"
 
 (****)
 
-let suites = ["oldstable"; "stable"; "testing"; "unstable"]
+let suites = ["oldstable"; "stable"; "testing"; "unstable"; "testing-proposed-updates"]
 let archs = ["i386"; "sparc"; "powerpc"; "armel"; "ia64"; "mips"; "mipsel"; "s390"; "amd64"; "kfreebsd-i386"; "kfreebsd-amd64"]
 let sects = ["main"; "contrib"; "non-free"]
 let ext = "bz2"
 
 let download dst url =
+  let tmp = dst ^ ".tmp" in
   let cmd =
     if Sys.file_exists dst then
-      Format.sprintf "curl -# -z %s --create-dirs -o %s %s" dst dst url
+      Format.sprintf "curl -f -# -z %s --create-dirs -o %s %s" dst tmp url
     else
-      Format.sprintf "curl -# --create-dirs -o %s %s" dst url
+      Format.sprintf "curl -f -# --create-dirs -o %s %s" dst url
   in
   Format.printf "> %s@." cmd;
-  ignore (Sys.command cmd)
+  if Sys.command cmd = 0 && Sys.file_exists tmp then begin
+    let cmd = Format.sprintf "mv %s %s" tmp dst in
+    Format.printf "> %s@." cmd;
+    ignore (Sys.command cmd)
+  end
 
 let rec make_directories f =
   let f = Filename.dirname f in
@@ -88,7 +93,7 @@ let _ =
             (fun s -> (Format.sprintf "binary-%s/Packages.%s" s ext,
                        Format.sprintf "Packages_%s" s))
             archs))
-    ["testing"; "unstable"]
+    ["testing"; "unstable"; "testing-proposed-updates"]
 
 (****)
 
@@ -112,7 +117,7 @@ List.iter
 
 let hint_files =
   ["aba"; "adsb"; "faw"; "he"; "jcristau"; "luk"; "mehdi";
-   "neilm"; "nthykier"; "pkern"; "vorlon"; "zobel";
+   "neilm"; "nthykier"; "pkern"; "vorlon";
    "freeze"; "freeze-exception"]
 
 let hint_src = "http://release.debian.org/britney/hints/"
